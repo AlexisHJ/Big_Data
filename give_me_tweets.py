32565e1872f14de891fadf56c2720d21b54e1507
AlexisHJ <90406020+AlexisHJ@users.noreply.github.com>
2022-03-16 16:59:29 -0500

    Se edita por seguridad

--- give_me_tweets.py
+++ give_me_tweets.py
@@ -1,10 +1,10 @@
 import credentials
 import tweepy
 
-API_KEY = "gq1Cjyyvi5E1DZaeFlcYaisjP" 
-API_SECRET_KEY = "T8FqcCEIG31EH2ZEJz8aUk0kvVRxRGGWIrdlk2unIbFiloyYrD"
-ACCESS_TOKEN = "1388888268020781059-sQmy1ULdpqKzH5ACZjb8qfKw8I9vbo"
-ACCESS_TOKEN_SECRET = "e6NDINWYZXmyGYXA2VbFiYUXjvfYSN4U41Oqykrh29ZXj"
+API_KEY = "" 
+API_SECRET_KEY = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
 
 auth = tweepy.OAuthHandler(credentials.API_KEY, credentials.API_SECRET_KEY)
 auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
